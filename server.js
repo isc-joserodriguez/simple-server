399d11e520ce08617fe628cdc0aafbbb09575e32
José Rodriguez <isc.joserodriguez@gmail.com>
2023-05-24 00:07:55 -0700

    Simple Server

---
+++ server.js
@@ -0,0 +1,127 @@
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+
+// Configura la conexión a MongoDB
+mongoose
+  .connect(
+    'mongodb+srv://testMongo:testMongo@cluster0.h81et.mongodb.net/c17-API',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log('Conexión exitosa a MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error al conectar a MongoDB', error);
+  });
+
+// Definir el esquema y modelo de usuario
+const UserSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String,
+});
+
+const User = mongoose.model('User', UserSchema);
+
+// Configuración de Express para manejar solicitudes JSON
+app.use(express.json());
+
+// Ruta de registro de usuario
+app.post('/register', async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    // Verifica si el usuario ya existe en la base de datos
+    if (existingUser) {
+      return res.status(400).json({ error: 'El usuario ya existe' });
+    }
+    // Crea un nuevo usuario
+    const user = new User({ firstName, lastName, email, password });
+
+    const createdUser = await user.save();
+    if (createdUser) {
+      res.json({ message: 'Usuario registrado exitosamente' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+});
+
+// Ruta de inicio de sesión
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Verifica las credenciales del usuario
+    const user = await User.findOne({ email, password });
+    if (!user) {
+      return res.status(401).json({ error: 'Credenciales inválidas' });
+    }
+
+    // Genera un token JWT válido por 1 hora
+    const token = jwt.sign(
+      {
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+      },
+      'secretKey',
+      { expiresIn: '1h' }
+    );
+
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+});
+
+// Ruta protegida por JWT
+app.get('/user', authenticateToken, (req, res) => {
+  res.json(req.user);
+});
+
+app.get('/users', authenticateToken, async (req, res) => {
+  try {
+    const users = await User.find();
+    // Verifica si el usuario ya existe en la base de datos
+    if (!users.length) {
+      return res.status(400).json({ error: 'No hay usuarios' });
+    }
+
+    res.json({ data: users });
+  } catch (error) {
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+});
+
+// Función de middleware para autenticar el token
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ error: 'Token de autenticación no provisto' });
+  }
+
+  jwt.verify(token, 'secretKey', (error, user) => {
+    if (error) {
+      return res.status(403).json({ error: 'Token de autenticación inválido' });
+    }
+
+    req.user = user;
+    next();
+  });
+}
+
+// Inicia el servidor
+app.listen(3000, () => {
+  console.log('Servidor iniciado en el puerto 3000');
+});
